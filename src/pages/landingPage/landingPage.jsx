9160335cee3b3e4b958861f8b6d831b21d842a72
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2023-09-19 23:26:51 +0200

    implémentation effet goo sur le composant LandingPage

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -13,17 +13,20 @@ function LandingPage() {
 
     useEffect(()=> {
         addCSSClass()
-    },[])
+    }, [])
 
     return (
         <>
+        <div className='landingPage__container'>
+            <div className='toBeCrossed'></div>
             <div 
                 ref = {container}
-                className='landingPage__container'
+                className='landingPage__content'
             >
                 <h1>Arnaud Goguelin | Développeur web</h1>
                 <p>    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. !</p>
             </div>
+        </div>
         </>
     )
 }
