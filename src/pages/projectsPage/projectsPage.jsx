ab5655d2a1147fe8fe17b6f24a46af7f1766b682
Arnaud Goguelin <arnaud.goguelin@gmail.com>
2023-09-23 03:00:37 +0200

    mise en place des filtres des projets

--- src/pages/projectsPage/projectsPage.jsx
+++ src/pages/projectsPage/projectsPage.jsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 
 import './projectsPage.scss'
 
@@ -13,7 +13,10 @@ function ProjectsPage() {
     const scrollBarToFill = useRef(null)
     const movingGallery = useRef(null)
 
-    
+    //création d'un variable contenant les projets filtrés
+    const [ projectsFiltered, setProjectsFiltered ] = useState([])
+
+    //Création d'un tableau de valeurs uniques pour générer les boutons filtres
     const unusableTags = projectsData.map((project) => project.tags);
     const tagsWithDuplicates = unusableTags.reduce((acc, tags) => {
         return acc.concat(tags)
@@ -21,14 +24,21 @@ function ProjectsPage() {
     const tags = Array.from(new Set(tagsWithDuplicates))
     tags.unshift('Tous')
 
-    function handleClick(event) {
+    //Identification du filtre actif + filtrage des projets
+    function filterProjects(event) {
+        //Ajout et suppression de classes CSS pour identifier le filtre actif
         const allButtonsFilter = Array.from(buttonsContainer.current.children)
         allButtonsFilter.forEach(buttonFilter => buttonFilter.classList.remove('active'))
 
         const selectedButton = event.target
         selectedButton.classList.add('active')
-    }
 
+        //Filtre des projetcs en fonction de la valeur du bouton actif et mise à jour de la variable projectsFiltered 
+        const filterValue = event.target.innerText
+        
+        setProjectsFiltered(projectsData.filter(project => project.tags.includes(filterValue)))
+    }
+    //Gestion de l'évènement wheel
      function handleWheel(event) {
         const gallery = movingGallery.current;
         // On récupère la valeur de scrollLeft de gallery 
@@ -36,7 +46,6 @@ function ProjectsPage() {
         // et on y ajoute deltaY (la quantité de défilement vertical dans l'unité)
         // verticale car tous les utilisateurs ne disposent pas de souris avec molettes horizontales.
         return gallery.scrollLeft += event.deltaY;
-        
     }   
 
     return (
@@ -54,7 +63,7 @@ function ProjectsPage() {
                     key={'filter n°' + tags.indexOf(tag)}
                     className='projectsPage__filter'
                     content={tag}
-                    onClick={handleClick}
+                    onClick={filterProjects}
                 />
             ))
             }
@@ -63,10 +72,21 @@ function ProjectsPage() {
             <div
                 className='projectsPage__gallery'
                 ref={movingGallery}
-                // onKeyDown={ (event) => handleKeyDown(event)}
             >
-                {
+                {   //Condition d'affichge des projet: si le tableau des projets filtrés vide,
+                    //càd au premier affichage sans aucun filtre actif, alors on utilise le tableau de données complet,
+                    //sinon s'il est rempli, c'est qu'un filtre est actif, et on affiche les projets filtrés. 
+                    projectsFiltered.length === 0 ?
                     projectsData.map(({id, title, imageURL}) => (
+                        <ProjectCard
+                        key={id}
+                        id={id}
+                        title={title}
+                        imageURL ={imageURL}
+                        />
+                    ))
+                    :
+                    projectsFiltered.map(({id, title, imageURL}) => (
                         <ProjectCard
                         key={id}
                         id={id}
